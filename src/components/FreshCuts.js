1058099246fb9f404f18c1c068ca16eb3d03fca6
jackie <jacquelyn.cheng@me.com>
2021-02-27 09:52:11 -0800

    Add photos to firebase and pull user data from firebase into card deck

--- src/components/FreshCuts.js
+++ src/components/FreshCuts.js
@@ -1,18 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import firebase from 'firebase/app'
 
-// import TinderCard from '../react-tinder-card/index'
 import TinderCard from 'react-tinder-card'
 import ReactModal from 'react-modal';
 
 import FreshCut from '../data/FreshCut'
 
-var db = require('../data/prof.json').profiles
-console.log(db)
-
 function FreshCuts () {
-  const characters = db
-  const [lastDirection, setLastDirection] = useState()
+  const [swipeFeedback, setSwipeFeedback] = useState()
   const [freshCuts, setFreshCuts] = useState([])
   const [showExpanded, setShowExpanded] = useState(false);
   const [profID, setProfID] = useState(0);
@@ -26,7 +21,7 @@ function FreshCuts () {
           var users = snapshot.toJSON();
           for (var uuid in users) {
               var u = users[uuid];
-              cuts.push(new FreshCut(uuid, u.username, u.city, u.state, u.company, u.bio));
+              cuts.push(new FreshCut(uuid, u.username, u.firstname, u.photo, u.age, u.city, u.company, u.bio));
           }
           console.log("Fetched the following users: " + JSON.stringify(cuts));
           setFreshCuts(cuts);
@@ -35,7 +30,8 @@ function FreshCuts () {
 
   const swiped = (direction, nameToDelete) => {
     console.log('removing: ' + nameToDelete)
-    setLastDirection(direction)
+    var feedback = direction == 'down' || direction == "left" ? "Hard no on " + nameToDelete : nameToDelete + " is pretty tasty!";
+    setSwipeFeedback(feedback);
   }
 
   const outOfFrame = (name) => {
@@ -49,37 +45,32 @@ function FreshCuts () {
 
   return (
     <div>
-      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
-      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
-
-      {/* example of how you would use data from firebase: */}
-      {/* <h1>{freshCuts.length != 0 && freshCuts[0].username}</h1> */}
-
-      <div className='cardContainer'>
-        <p>Fresh cuts for {firebase.auth().currentUser.displayName}</p>
-        {characters.map((character) =>
-          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-              <h3>{character.name}</h3>
-              <h4 onClick={() => clickMore(character)}>See more</h4>
-              <ReactModal
-                className="Modal"
-                overlayClassName="Overlay"
-                isOpen={showExpanded && profID==character.id}
-                contentLabel="example"
-              >
-                <img className="photo" src={character.url}/>
-                <h2 className="Modal-h2">{character.name}, age</h2>
-                <h3>City</h3>
-                <h3>Favorite Meat</h3>
-                <button onClick={() => setShowExpanded(false)}>Close</button>
-              </ReactModal>
-            </div>
+      <div className="cardContainer">
+        <p>Mmm, fresh cuts just for you.</p>
+        {freshCuts.length != 0 && freshCuts.map((freshCut) =>
+          <TinderCard className="swipe" key={freshCut.username} onSwipe={
+            (dir) => swiped(dir, freshCut.firstname)} onCardLeftScreen={() => outOfFrame(freshCut.username)}>
+              <div style={{ backgroundImage: "url('" + freshCut.photo + "')" }} className="card">
+                <h3>{freshCut.username}</h3>
+                <h4 onClick={() => clickMore(freshCut)}>See more</h4>
+                <ReactModal
+                  className="Modal"
+                  overlayClassName="Overlay"
+                  isOpen={showExpanded && profID==freshCut.uuid}
+                  contentLabel="example"
+                >
+                  <img className="photo" src={freshCut.photo}/>
+                  <h2 className="Modal-h2">{freshCut.username}, {freshCut.age}</h2>
+                  <h3>City: {freshCut.city}</h3>
+                  <h3>Favorite Meat: {freshCut.cut}</h3>
+                  <button onClick={() => setShowExpanded(false)}>Close</button>
+                </ReactModal>
+              </div>
           </TinderCard>
         )}
       </div>
       <br/>
-      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+      {swipeFeedback && <h2 className='infoText'>{swipeFeedback}</h2>}
     </div>
   )
 }
